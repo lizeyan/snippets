5053a533398dbb9afec018ee10092cf21934a920
Zeyan LI <zy-li14@outlook.com>
2018-07-25 18:59:55 +0800

    coverage

--- snippets/modules/lambda_module.py
+++ snippets/modules/lambda_module.py
@@ -2,10 +2,23 @@ import torch.nn as nn
 import typing
 
 
-class Lambda(nn.ModuleDict):
+class Lambda(nn.Module):
+    """
+    Not that only function without any tensor parameters are suitable,
+    """
     def __init__(self, func: typing.Callable):
         super().__init__()
         self.func = func
 
     def forward(self, *args, **kwargs):
         return self.func(*args, **kwargs)
+
+    def cuda(self, device=None):
+        if isinstance(self.func, nn.Module):
+            self.func.cuda(device)
+        return self
+
+    def cpu(self):
+        if isinstance(self.func, nn.Module):
+            self.func.cpu()
+        return self
