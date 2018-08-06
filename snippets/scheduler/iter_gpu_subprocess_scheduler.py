f28840c95641052840d062d1e8781f14d15c9064
lizytalk <lizytalk@outlook.com>
2018-08-06 13:46:17 +0800

    add restart to iter_gpu_scheduler

--- snippets/scheduler/iter_gpu_subprocess_scheduler.py
+++ snippets/scheduler/iter_gpu_subprocess_scheduler.py
@@ -7,20 +7,29 @@ from ..scaffold import get_gpu_metrics
 
 
 class IterGPUSubprocessScheduler(object):
-    def __init__(self, devices: Union[str, Iterable[int]]="all", interval: float=1):
+    def __init__(self, devices: Union[str, Iterable[int]]="all",
+                 interval: float=1,
+                 restart_failed: bool=False):
         if devices == "all":
             devices = list(int(_) for _ in get_gpu_metrics()["index"])
         self._device2job = {device: None for device in devices}
         assert len(self._device2job) > 0, "No GPU device given"
         self._interval = interval
+        self._restart_failed = restart_failed
 
         self._job_args_list = []
         self._next_start_job_index = 0
         self._available_device = list(self._device2job.keys())[0]
         self._future_list = []
         self._finished_cnt = 0
+        self._failed_job_indices = set()
 
         self.__called = False
+
+    @property
+    def failed_count(self):
+        assert self.__called, "This scheduler has not run."
+        return len(self._failed_job_indices)
 
     def submit(self, *args, **kwargs):
         self._job_args_list.append((args, kwargs))
@@ -32,7 +41,7 @@ class IterGPUSubprocessScheduler(object):
         assert not self.__called, "It can't be called again, create a new object"
         while self._has_unfinished_jobs() or self._has_pending_jobs():
             while self._has_avail_device() and self._has_pending_jobs():
-                self._start_job()
+                self._start_next_job()
             time.sleep(self._interval)
         self.__called = True
 
@@ -48,12 +57,20 @@ class IterGPUSubprocessScheduler(object):
             index = self._device2job[key]["index"]
             setting = self._job_args_list[index]
             if job.poll() is not None:
-                logging.getLogger(__file__).info(f"job at device {key} exits with {job.poll()}: {setting}")
+                logging.getLogger(__file__).info(f"job {index} at device {key} exits with {job.poll()}: {setting}")
                 self._finished_cnt += 1
-                logging.getLogger(__file__).info(f"{self._finished_cnt}/{len(self._job_args_list)} ended")
                 self._future_list[index]["stdout"] = job.stdout
                 self._future_list[index]["stderr"] = job.stderr
                 self._device2job[key] = None
+                if job.poll() is not 0:
+                    self._failed_job_indices.add(index)
+                    if self._restart_failed:
+                        job.stdout.close() if job.stdout is not None else None
+                        job.stderr.close() if job.stderr is not None else None
+                        self._restart_job(index, key)
+                else:
+                    if index in self._failed_job_indices:
+                        self._failed_job_indices.remove(index)
 
     def _has_pending_jobs(self) -> bool:
         return self._next_start_job_index is not None and self._next_start_job_index < len(self._job_args_list)
@@ -66,20 +83,25 @@ class IterGPUSubprocessScheduler(object):
                 return True
         return False
 
-    def _start_job(self):
+    def _start_next_job(self):
         assert self._available_device is not None and self._next_start_job_index < len(self._job_args_list)
-        args, kwargs = self._job_args_list[self._next_start_job_index]
+        self._start_job(self._next_start_job_index, self._available_device)
+        logging.getLogger(__file__).info(f"{self._next_start_job_index + 1}/{len(self._job_args_list)} started")
+        self._next_start_job_index += 1
+        self._available_device = None
+
+    def _start_job(self, index, device):
+        args, kwargs = self._job_args_list[index]
         if "env" in kwargs:
             env = kwargs["env"]
         else:
             env = os.environ
-        env["CUDA_VISIBLE_DEVICES"] = f"{self._available_device}"
+        env["CUDA_VISIBLE_DEVICES"] = f"{device}"
         kwargs["env"] = env
         job = Popen(*args, **kwargs)
-        self._device2job[self._available_device] = {"job": job, "index": self._next_start_job_index}
+        self._device2job[device] = {"job": job, "index": index}
         logging.getLogger(__file__).info(f"job assigned to {self._available_device}: {args, kwargs}")
-        self._available_device = None
-        self._next_start_job_index += 1
-        logging.getLogger(__file__).info(f"{self._next_start_job_index}/{len(self._job_args_list)} started")
 
-
+    def _restart_job(self, index, key):
+        self._start_job(index, key)
+        logging.getLogger(__file__).info(f"{index + 1}/{len(self._job_args_list)} restarted")
--- tests/scheduler/test_iter_gpu_subprocess_scheduler.py
+++ tests/scheduler/test_iter_gpu_subprocess_scheduler.py
@@ -12,7 +12,7 @@ logging.basicConfig(
 
 
 class TestIterGOUSubprocessScheduler(unittest.TestCase):
-    def test(self):
+    def test_run(self):
         scheduler = IterGPUSubprocessScheduler(devices="all", interval=0.001)
         future1 = scheduler.submit("ls /tmp -al", stdout=subprocess.PIPE, shell=True)
         scheduler.submit("cd /tmp && cd ..", shell=True)
@@ -22,3 +22,26 @@ class TestIterGOUSubprocessScheduler(unittest.TestCase):
         self.assertTrue(future1["stdout"] is not None)
         self.assertTrue(future1["stderr"] is None)
         future1["stdout"].close()
+
+    def test_failed(self):
+        scheduler = IterGPUSubprocessScheduler(devices="all", interval=0.001)
+        future1 = scheduler.submit("cd /data", stderr=subprocess.PIPE, shell=True, env={})
+        scheduler()
+        self.assertTrue(future1["stdout"] is None)
+        self.assertTrue(future1["stderr"] is not None)
+        future1["stderr"].close()
+        self.assertEqual(scheduler.failed_count, 1)
+
+    def test_restart(self):
+        scheduler = IterGPUSubprocessScheduler(devices="all", interval=0.001, restart_failed=True)
+        future1 = scheduler.submit(" if [ ! -f /tmp/foo.txt ]; then touch /tmp/foo.txt && exit 1; \
+                    else rm /tmp/foo.txt && exit 0; fi",
+                                   stderr=subprocess.PIPE, shell=True, env={})
+        scheduler()
+        self.assertTrue(future1["stdout"] is None)
+        self.assertTrue(future1["stderr"] is not None)
+        future1["stderr"].close()
+        self.assertEqual(scheduler.failed_count, 0)
+        self.assertEqual(scheduler._failed_job_indices, set())
+        self.assertEqual(scheduler._available_device, None)
+        self.assertEqual(scheduler._next_start_job_index, 1)
